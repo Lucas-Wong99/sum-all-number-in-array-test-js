bab527057f78858926871c05c91e76f709eda400
Lucas Wong <lucaswong.ca@gmail.com>
2020-05-02 22:05:52 +0000

    completed the work

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  let results = 0;
+  for (const i of array) {
+    if (Array.isArray(i)) {
+      results += sumItems(i);
+    } else {
+      results += i;
+    }
+  }
+  return results;
 }
 
 module.exports = sumItems;
+
